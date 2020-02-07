ba6311d8bf800190e3ed5b793d40eb7daa5096e2
ArianeBeaudin <arianeb@localhost.localdomain>
2020-02-07 09:21:46 -0500

    gestion des apostrophes but still lack of saut de ligne

--- remplir_lettre.sh
+++ remplir_lettre.sh
@@ -1,6 +1,6 @@
 echo -e 'NOM DU DEPUTE \nSON ADRESSE' > output_remplir_texte.txt
 #cat lettre_test.txt >> output_remplir_texte.txt
-sed "s/\<Demande de financement pour un comité d'environnement>/& $1/" Lettre-a-remplir.txt >> output_remplir_texte.txt
+sed -r " s/\<Demande de financement pour un comité d\’environnement\>/& $1/" Lettre-a-remplir.txt >> output_remplir_texte.txt
 
 
 
