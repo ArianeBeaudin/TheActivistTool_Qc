cd0f0d81be1478b8bdab95ed805f566e6bc1152e
ArianeBeaudin <arianeb@localhost.localdomain>
2020-02-07 12:47:48 -0500

    new technique with sed working

--- script/remplir_lettre.sh
+++ script/remplir_lettre.sh
@@ -1,11 +1,21 @@
 echo -e 'NOM DU DEPUTE \nSON ADRESSE' > test_texte.txt
+
+sed -i "s/INSERT1/$1/g" lettre_modele.txt
+
+sed -i "s/INSERT2/$2/g" lettre_modele.txt
+
+
+
+
+
+
 #sed -r " s/\<Demande de financement pour un comité d\’environnement\>/& $1/" ../references/Lettre-Député-sans-texte.txt  >> test_texte.txt
 
 #sed -r " s/\<attention de \>/& $1/" ../references/Lettre-Député-sans-texte.txt  >> test_texte.txt
 
-sed -r " s/\<Je m\’appelle\>/& $2/" ../references/Lettre-Député-sans-texte.txt  >> test_texte.txt
+#sed -r " s/\<Je m\’appelle\>/& $2/" ../references/Lettre-Député-sans-texte.txt  >> test_texte.txt
 
-sed -r " s/\<agréer,\>/& $3/" test_texte.txt >> test_texte.txt
+#sed -r " s/\<agréer,\>/& $3/" test_texte.txt >> test_texte.txt
 
 #sed -r " s/\<agréer, \>/& Louis/" test_texte.txt
 
