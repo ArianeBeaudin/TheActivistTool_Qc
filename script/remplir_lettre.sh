3a4db4476bb804131750136d90138eb238b83d8d
ArianeBeaudin <arianeb@localhost.localdomain>
2020-02-07 10:08:37 -0500

    gitignore

--- script/remplir_lettre.sh
+++ script/remplir_lettre.sh
@@ -1,6 +1,16 @@
 echo -e 'NOM DU DEPUTE \nSON ADRESSE' > test_texte.txt
-sed -r " s/\<Demande de financement pour un comité d\’environnement\>/& $1/" ../references/Lettre-Député-sans-texte.txt  >> test_texte.txt
+#sed -r " s/\<Demande de financement pour un comité d\’environnement\>/& $1/" ../references/Lettre-Député-sans-texte.txt  >> test_texte.txt
 
+#sed -r " s/\<attention de \>/& $1/" ../references/Lettre-Député-sans-texte.txt  >> test_texte.txt
+
+sed -r " s/\<Je m\’appelle\>/& $2/" ../references/Lettre-Député-sans-texte.txt  >> test_texte.txt
+
+sed -r " s/\<agréer,\>/& $3/" test_texte.txt >> test_texte.txt
+
+#sed -r " s/\<agréer, \>/& Louis/" test_texte.txt
+
+
+#cat lettre_test.txt >> output_remplir_texte.txt
 
 
 #sed "s/\<nom est\>/& $1/" output_remplir_texte.txt > output_final.txt
