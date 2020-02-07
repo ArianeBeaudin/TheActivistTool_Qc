99b924d78480091dd4e5ccc8227e83f32f0ec0f9
ArianeBeaudin <arianeb@localhost.localdomain>
2020-02-07 09:40:09 -0500

    path working

--- script/remplir_lettre.sh
+++ script/remplir_lettre.sh
@@ -1,6 +1,5 @@
-echo -e 'NOM DU DEPUTE \nSON ADRESSE' > output_remplir_texte.txt
-#cat lettre_test.txt >> output_remplir_texte.txt
-sed -r " s/\<Demande de financement pour un comité d\’environnement\>/& $1/" Lettre-a-remplir.txt >> output_remplir_texte.txt
+echo -e 'NOM DU DEPUTE \nSON ADRESSE' > test_texte.txt
+sed -r " s/\<Demande de financement pour un comité d\’environnement\>/& $1/" ../references/Lettre-Député-sans-texte.txt  >> test_texte.txt
 
 
 
